26158d7627083cfbaca1cb46f858d8c5a9c48552
Sourin <sourin.mukherjee2580@gmail.com>
2024-02-06 08:14:36 +0530

    modified some files

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
@@ -13,7 +13,7 @@ st.set_page_config(
 )
 
 # model = load_model(r'C:\Python\Stock\Stock Predictions Model.keras')
-model = load_model('Stock Predictions Model.keras')
+model = load_model('Stock Predictions Model.h5')
 
 
 st.header('Stock Market Predictor')
